fe2f61386e3a82ed1757b2d62bc8100e2a493cba
Naresh Mahapatra <codes.xdev@gmail.com>
2025-09-14 14:25:02 +0530

    some changes checks

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './Components/Footer.jsx';
 import Home from './Pages/Home.jsx';
 import Skill from './Pages/Skill.jsx';
-
+import Project from './Pages/Project.jsx';
 function App() {
 
   return (
@@ -14,6 +14,7 @@ function App() {
       <Navbar />
       <Home />
       <Skill />
+      <Project />
       {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
